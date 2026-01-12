854e81362a058304de883a1abb1af57091836530
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-01-12 16:54:41 +0200

    condition by CUDA version

--- CMake/cuda_config.cmake
+++ CMake/cuda_config.cmake
@@ -13,15 +13,38 @@ message(STATUS "CUDA_LIBRARIES: ${CUDA_INCLUDE_DIRS} ${ALL_CUDA_LIBS}")
 set(CUDA_PROPAGATE_HOST_FLAGS OFF)
 set(CUDA_SEPARABLE_COMPILATION ON)
 
+# Build CUDA architecture list based on CUDA version
+if(CUDA_VERSION VERSION_LESS "13.0")
+    set(CUDA_ARCH_LIST 62)  # Pascal (CUDA 8.0-12.x, dropped in CUDA 13.0)
+else()
+    set(CUDA_ARCH_LIST)
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "10.0")
+    list(APPEND CUDA_ARCH_LIST 75)  # Turing
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "11.0")
+    list(APPEND CUDA_ARCH_LIST 80 86)  # Ampere
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "11.8")
+    list(APPEND CUDA_ARCH_LIST 89)  # Ada Lovelace
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "12.0")
+    list(APPEND CUDA_ARCH_LIST 90)  # Hopper
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "12.6")
+    list(APPEND CUDA_ARCH_LIST 110 120)  # Jetson Thor
+endif()
+
 # Check if variable is available (means CMake >= 3.18)
 if(POLICY CMP0104)
     # Use modern approach
     cmake_policy(SET CMP0104 NEW)
-    # Set your target architectures below. Example: Turing and Ampere
-    set(CMAKE_CUDA_ARCHITECTURES 62 75 80 110 120)
+    set(CMAKE_CUDA_ARCHITECTURES ${CUDA_ARCH_LIST})
 else()
-    # Fallback for older CMake: set the classical NVCC flags
-    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_62,code=sm_62 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_110,code=sm_110 -gencode arch=compute_120,code=sm_120")
+    # Fallback for older CMake: build NVCC flags from architecture list
+    foreach(ARCH ${CUDA_ARCH_LIST})
+        set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_${ARCH},code=sm_${ARCH}")
+    endforeach()
 endif()
 
 
