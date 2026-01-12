36bb0d0ea6a636e054578546047cca1c605edbd0
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-01-12 15:55:14 +0200

    build with CUD flags

--- CMake/cuda_config.cmake
+++ CMake/cuda_config.cmake
@@ -12,4 +12,16 @@ message(STATUS "CUDA_LIBRARIES: ${CUDA_INCLUDE_DIRS} ${ALL_CUDA_LIBS}")
 
 set(CUDA_PROPAGATE_HOST_FLAGS OFF)
 set(CUDA_SEPARABLE_COMPILATION ON)
-set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS}; -O3 -gencode arch=compute_53,code=sm_53 -gencode arch=compute_62,code=sm_62;")
+
+# Check if variable is available (means CMake >= 3.18)
+if(POLICY CMP0104)
+    # Use modern approach
+    cmake_policy(SET CMP0104 NEW)
+    # Set your target architectures below. Example: Turing and Ampere
+    set(CMAKE_CUDA_ARCHITECTURES 62 75 80 110 120)
+else()
+    # Fallback for older CMake: set the classical NVCC flags
+    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_62,code=sm_62 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_110,code=sm_110 -gencode arch=compute_120,code=sm_120")
+endif()
+
+
