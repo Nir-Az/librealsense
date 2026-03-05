0bfd217a2d6e452597953ddc41da95f740a9b4a7
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2026-03-05 17:51:28 +0200

    mark viewer UT as nightly

--- unit-tests/live/tools/test-realsense-viewer.py
+++ unit-tests/live/tools/test-realsense-viewer.py
@@ -2,6 +2,7 @@
 # Copyright(c) 2026 Intel Corporation. All Rights Reserved.
 
 #test:device D400*
+#test:donotrun:!nightly
 
 import os, platform, shutil, subprocess
 from rspy import log, repo, test
