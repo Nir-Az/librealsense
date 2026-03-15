a575bfbb94420ba4105a8d8a9661e78165d07017
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-03-15 12:24:16 +0200

    Inline _find_flag into _consume_flag_with_arg

--- unit-tests/py/rspy/pytest/cli.py
+++ unit-tests/py/rspy/pytest/cli.py
@@ -6,26 +6,19 @@
 import sys
 
 
-def _find_flag(flag):
-    """Find a flag in sys.argv, returning its index or None."""
-    try:
-        return sys.argv.index(flag)
-    except ValueError:
-        return None
-
-
 def _consume_flag_with_arg(flags, pytest_equiv):
     """Consume a flag+argument from sys.argv, translate to pytest equivalent."""
     for flag in flags:
-        idx = _find_flag(flag)
-        if idx is not None:
-            if idx + 1 >= len(sys.argv):
-                print(f'-F- {flag} requires an argument', file=sys.stderr)
-                sys.exit(1)
-            value = sys.argv[idx + 1]
-            del sys.argv[idx:idx + 2]
-            sys.argv.extend([pytest_equiv, value])
-            return value
+        if flag not in sys.argv:
+            continue
+        idx = sys.argv.index(flag)
+        if idx + 1 >= len(sys.argv):
+            print(f'-F- {flag} requires an argument', file=sys.stderr)
+            sys.exit(1)
+        value = sys.argv[idx + 1]
+        del sys.argv[idx:idx + 2]
+        sys.argv.extend([pytest_equiv, value])
+        return value
     return None
 
 
