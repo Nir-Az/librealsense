4e8e9d7f589f7ae9133d340b1ff573f08f688625
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-03-18 19:09:21 +0200

    Shorten device discovery log and reduce debug indent to 2 spaces

--- unit-tests/py/rspy/devices.py
+++ unit-tests/py/rspy/devices.py
@@ -289,7 +289,7 @@ def query( monitor_changes=True, hub_reset=False, recycle_ports=True, disable_dd
                 detected_sns.add(sn)
                 device = Device( sn, dev )
                 _device_by_sn[sn] = device
-                log.d( '... port {}:'.format( device.port is None and '?' or device.port ), sn, dev, 'detected and added to devices list' )
+                log.d( f'... port {device.port or "?"}: {sn} {dev}' )
 
                 name = dev.get_info(rs.camera_info.name) if dev.supports(rs.camera_info.name) else ""
                 d555_found = "D555" in name        
