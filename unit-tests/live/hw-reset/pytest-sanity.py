5beddfdecc97a2dea1694826b6d0ce9bc86aa1af
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-04-15 12:34:34 +0300

    pytest-sanity: cache target serial to avoid get_info on removed device handle

--- unit-tests/live/hw-reset/pytest-sanity.py
+++ unit-tests/live/hw-reset/pytest-sanity.py
@@ -17,6 +17,7 @@ pytestmark = [
 ]
 
 dev = None
+target_sn = None   # cached once before hardware_reset() - the removed dev handle cannot be queried safely
 device_removed = False
 device_added = False
 device_removed_time = 0
@@ -24,19 +25,20 @@ device_added_time = 0
 
 def device_changed( info ):
     global dev, device_removed, device_added, device_removed_time, device_added_time
-    if info.was_removed(dev):
+    if dev and info.was_removed( dev ):
         device_removed_time = time.perf_counter()
         device_removed = True
     for new_dev in info.get_new_devices():
-        added_sn = new_dev.get_info(rs.camera_info.serial_number)
-        tested_dev_sn = dev.get_info(rs.camera_info.serial_number)
-        if added_sn == tested_dev_sn:
-            device_added_time = time.perf_counter()
-            device_added = True
+        try:
+            if new_dev.get_info( rs.camera_info.serial_number ) == target_sn:
+                device_added_time = time.perf_counter()
+                device_added = True
+        except RuntimeError:
+            continue
 
 
 def test_hw_reset_sanity( test_device ):
-    global dev, device_removed, device_added, device_removed_time, device_added_time
+    global dev, target_sn, device_removed, device_added, device_removed_time, device_added_time
     device_removed = False
     device_added = False
     device_removed_time = 0
@@ -44,6 +46,7 @@ def test_hw_reset_sanity( test_device ):
 
     t = Timer( 10 )
     dev, ctx = test_device
+    target_sn = dev.get_info( rs.camera_info.serial_number )
     ctx.set_devices_changed_callback( device_changed )
     time.sleep(1)
     log.info( "Sending HW-reset command" )
