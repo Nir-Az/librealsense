d06fcb6231a1eaff08df50876926827fb157643a
Remi Bettan <remi.bettan@realsenseai.com>
2026-04-23 10:36:14 +0300

    migrating mipi-motion test

--- unit-tests/live/d400/pytest-mipi-motion.py
+++ unit-tests/live/d400/pytest-mipi-motion.py
@@ -1,39 +1,47 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2024 RealSense, Inc. All Rights Reserved.
 
-# test:device:jetson D457
-# test:donotrun:!jetson
-# test:retries 3
-
+import pytest
+import platform
 import pyrealsense2 as rs
-from rspy import test
+import logging
+log = logging.getLogger(__name__)
 import time
+
+pytestmark = [
+    pytest.mark.device("D457"),
+    pytest.mark.skipif(platform.machine() != "aarch64", reason="Jetson only"),
+    pytest.mark.flaky(retries=3),
+]
+
 gyro_frame_count = 0
 accel_frame_count = 0
 
-def frame_callback( f ):
-    global gyro_frame_count,accel_frame_count
-    stream_type = f.get_profile().stream_type()
-    if stream_type == rs.stream.gyro:
-        gyro_frame_count += 1
-        test.check_equal(f.get_frame_number(),gyro_frame_count)
-    elif stream_type == rs.stream.accel:
-        accel_frame_count += 1
-        test.check_equal(f.get_frame_number(), accel_frame_count)
 
-################################################################################################
-with test.closure("frame index - mipi IMU "):
+def test_frame_index_mipi_imu(test_device):
+    global gyro_frame_count, accel_frame_count
+    gyro_frame_count = 0
+    accel_frame_count = 0
+
+    def frame_callback(f):
+        global gyro_frame_count, accel_frame_count
+        stream_type = f.get_profile().stream_type()
+        if stream_type == rs.stream.gyro:
+            gyro_frame_count += 1
+            assert f.get_frame_number() == gyro_frame_count
+        elif stream_type == rs.stream.accel:
+            accel_frame_count += 1
+            assert f.get_frame_number() == accel_frame_count
+
     seconds_to_count_frames = 10
-    dev, _ = test.find_first_device_or_exit()
+    dev, _ = test_device
     sensor = dev.first_motion_sensor()
     motion_profile_accel = next(p for p in sensor.profiles if p.stream_type() == rs.stream.accel and p.fps() == 100)
-    motion_profile_gyro = next(p for p in sensor.profiles if p.stream_type() == rs.stream.gyro and p.fps()==100)
-    sensor.open( [motion_profile_accel, motion_profile_gyro] )
-    sensor.start( frame_callback )
-    time.sleep(seconds_to_count_frames) # Time to count frames
+    motion_profile_gyro = next(p for p in sensor.profiles if p.stream_type() == rs.stream.gyro and p.fps() == 100)
+    sensor.open([motion_profile_accel, motion_profile_gyro])
+    sensor.start(frame_callback)
+    time.sleep(seconds_to_count_frames)  # Time to count frames
     sensor.stop()
     sensor.close()
-    test.check(gyro_frame_count > 0)
-    test.check(accel_frame_count > 0)
-
-test.print_results_and_exit()
+    assert gyro_frame_count > 0
+    assert accel_frame_count > 0
