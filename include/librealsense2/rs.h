d9796d669ca8399d958fbd14e7e4a374ee05c1b2
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-09-17 17:33:56 +0300

    Update version to 2.59.1

--- include/librealsense2/rs.h
+++ include/librealsense2/rs.h
@@ -32,7 +32,7 @@ extern "C" {
 
 #define RS2_API_MAJOR_VERSION    2
 #define RS2_API_MINOR_VERSION    59
-#define RS2_API_PATCH_VERSION    0
+#define RS2_API_PATCH_VERSION    1
 #define RS2_API_BUILD_VERSION    0
 
 #ifndef STRINGIFY
